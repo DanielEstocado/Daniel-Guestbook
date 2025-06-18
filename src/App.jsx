2d0d70f493d0190bc68aef425de4ec7c3575deb5
Daniel Estocado <estocado.daniel143@gmail.com>
2025-06-18 11:53:45 +0800

    Border radius

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import UserEntries from './components/UserEntries.jsx'
 
 const App = () => {
   return (
-    <div className='flex max-w-3xl mx-auto min-h-screen flex-col pt-5 font-ubuntu text-neutral-600 overflow-x-hidden'>
+    <div className='flex max-w-3xl mx-auto h-screen flex-col py-5 font-ubuntu text-neutral-600 overflow-x-hidden'>
       {/* Header */}
-      <section className='border border-my-neutral p-6 rounded-t-xl '>
+      <section className='border border-my-neutral p-6 md:rounded-t-xl '>
         <h1 className='text-2xl font-bold'>Daniel's Guestbook</h1>
         <p className='text-lg'>You can leave your thoughts and comments below.</p>
       </section>
       {/* Main content */}
-      <section className='border-x border-my-neutral flex-grow p-6'>
+      <section className='border-x border-b md:rounded-b-xl border-my-neutral flex-grow p-6'>
         {/* Sign In */}
         <section className='border border-my-neutral p-6 rounded-sm'>
           <LoginOptions/>
