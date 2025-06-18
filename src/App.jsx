8fc9bb873048ecc9415e11d175c33eec67992c82
Daniel Estocado <estocado.daniel143@gmail.com>
2025-06-18 10:22:34 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex max-w-4xl bg-amber-400 mx-auto min-h-screen p-4'>
+      <h1 className='text-3xl font-bold'>Welcome to Daniel's Guestbook</h1>
+    </div>
+  )
+}
+
+export default App
