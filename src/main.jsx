4e9d01aa1e9a968368d0ac92fc663140e5867471
Daniel Estocado <estocado.daniel143@gmail.com>
2025-07-04 17:05:59 +0800

    use createRoutesFromElements in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements, } from 'react-router-dom'
 
 const router = createBrowserRouter(
-  [
-    { path: '/', element: <App /> },
-  ],
+  createRoutesFromElements(
+    <Route path="/" element={<App />} />
+  ),
   {
     basename: '/Daniel-Guestbook',
   }
-)
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
